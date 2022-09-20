e35f8d42369c281fe01bc5aba8161e618c2ed76f
Emma Forslund <emfo2102@student.miun.se>
2022-09-20 10:25:01 +0200

    Skapat App.vue

--- Moment 3 - Frontend-ramverk/src/App.vue
+++ Moment 3 - Frontend-ramverk/src/App.vue
@@ -1,11 +1,33 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <RouterView />
+  <Header />
+  <main>
+    <Quote />
+    <RouterView />
+  </main>
+  <Footer />
 </template>
 
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
+import Quote from "./components/Quote.vue";
+export default {
+  components: {
+    Header,
+    RouterView,
+    Footer,
+    Quote
+  }
+}
+</script>
+
 <style scoped>
+  main{
+    max-width: 1300px;
+    width: 100%;
+    margin: auto;
+  }
 
 </style>
