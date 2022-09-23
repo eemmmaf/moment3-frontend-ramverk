6e53c3541df344c5bf833783189c237e00a2733f
Emma Forslund <emfo2102@student.miun.se>
2022-09-23 10:10:33 +0200

    Skapat component för footer-elementet

---
+++ Moment 3 - Frontend-ramverk/src/components/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer>
+        <div class="footer-container">
+            <p>DT193G - Moment 3 - Frontend-ramverk</p>
+        </div>
+    </footer>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+footer{
+    background-color: #060506;
+    padding: 0.5rem;
+    margin-top: 3rem;
+}
+
+footer p{
+    font-family: 'Open-sans', sans-serif;
+    font-size: 1.8rem;
+    padding:1rem;
+}
+
+.footer-container {
+    max-width: 1200px;
+    width: 100%;
+    margin: auto;
+}
+
+.footer-container p {
+    color: white;
+    font-size: 1.8rem;
+    text-align: center;
+}
+</style>
