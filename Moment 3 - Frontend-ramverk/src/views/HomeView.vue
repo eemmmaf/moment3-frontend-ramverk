f59c9bd5905f68782abb9e4064917a04165b9feb
Emma Forslund <emfo2102@student.miun.se>
2022-09-29 10:47:43 +0200

    Skapat startsida

--- Moment 3 - Frontend-ramverk/src/views/HomeView.vue
+++ Moment 3 - Frontend-ramverk/src/views/HomeView.vue
@@ -1,8 +1,34 @@
-<script setup>
+<template>
+    <!--Flex-container med två artiklar om mitt intresse-->
+    <div class="section">
+        <div class="row">
+            <div class="grid-example col s12 m6">
+                <Article title="Vad är en podcast?"
+                    content="Poddradio, poddsändning eller podcast är en metod att publicera ljudfiler via internet. Den försvenskade kortformen podd förekommer också, men kan även avse en bärbar apparat för uppspelning av ljudfiler från internet. Källa: Wikipedia." />
+            </div>
+            <div class="grid-example col s12 m6">
+                <Article title="Mitt intresse"
+                    content="Att lyssna på podcast är perfekt att göra under dygnets alla timmar. Jag brukar främst göra det när jag gör olika vardagssysslor, som att laga mat eller diska. Det är även perfekt att lyssna på en podcast under en promenad. Jag tycker om flera olika genrer, men det jag främst lyssnar på är dokumentärer, podcasts med satiriska inslag och skräck." />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+//Importerar artikel-element-komponenten
+import Article from "../components/Article.vue";
+
+export default {
+    components: {
+        Article
+    }
+}
+
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style scoped>
+    .section{
+        margin-top: 4em;
+    }
+
+</style>
