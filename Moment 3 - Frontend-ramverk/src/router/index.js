6fa40d42210c42fb083c81b6ec20b665ae5cb5ee
Emma Forslund <emfo2102@student.miun.se>
2022-09-29 10:47:23 +0200

    Lagt till sökvägar i route

--- Moment 3 - Frontend-ramverk/src/router/index.js
+++ Moment 3 - Frontend-ramverk/src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../views/HomeView.vue';
+import FavoritesView from '../views/FavoritesView.vue';
+import About from '../views/About.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +10,16 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/mina-favoriter',
+      name: 'Mina favoriter',
+      component: FavoritesView
+    },
+    {
+      path: '/om-uppgiften',
+      name: 'Om uppgiften',
+      component: About
     }
   ]
 })
