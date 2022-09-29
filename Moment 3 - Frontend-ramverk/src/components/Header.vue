97578b579960a19dcd54423ac5ba406bef6e7beb
Emma Forslund <emfo2102@student.miun.se>
2022-09-29 14:10:52 +0200

    Lagt till kommentarer

--- Moment 3 - Frontend-ramverk/src/components/Header.vue
+++ Moment 3 - Frontend-ramverk/src/components/Header.vue
@@ -1,14 +1,19 @@
+<!--Komponent för header-->
 <template>
     <header>
+        <!--Navigering-->
         <nav>
             <div class="nav-wrapper white-text">
+                <!--Länk till startsidan-->
                 <router-link to="/">
-                    <h1 class="brand-logo">Mina podcasts</h1>
+                    <h1 class="brand-logo left">Mina podcasts</h1>
                 </router-link>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
+                <!--Ikonen för mobilmeny/hamburgermeny-->
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i
+                        class="material-icons">menu</i></a>
                 <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li>
-                        <router-link to="/mina-favoriter">Mina favoriter</router-link>
+                        <router-link to="/mina-favoriter">Mina favoritpodcasts</router-link>
                     </li>
                     <li>
                         <router-link to="/om-uppgiften">Om uppgiften</router-link>
@@ -16,15 +21,14 @@
                 </ul>
             </div>
         </nav>
-        <ul class="sidenav purple lighten-4" id="mobile-demo">
-            <li class="white-text">
-                <router-link to="/mina-favoriter">Mina favoriter</router-link>
+        <ul class="sidenav purple darken-4" id="mobile-demo">
+            <li>
+                <router-link to="/mina-favoriter" class="white-text">Mina favoritpodcasts</router-link>
             </li>
-            <li class="white-text">
-                <router-link to="/om-uppgiften">Om uppgiften</router-link>
+            <li>
+                <router-link to="/om-uppgiften" class="white-text">Om uppgiften</router-link>
             </li>
         </ul>
-        <Quote />
         <img src="../assets/microphone.jpg" alt="mikrofon framför svart bakgrund">
     </header>
 
@@ -32,14 +36,13 @@
 
 <script>
 import { RouterLink } from 'vue-router';
-import Quote from '../components/Quote.vue';
 export default {
     components: {
-        RouterLink,
-        Quote
+        RouterLink
     }
 }
 
+//JS-kod från Materialize för hamburger-menyn
 document.addEventListener('DOMContentLoaded', function () {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
