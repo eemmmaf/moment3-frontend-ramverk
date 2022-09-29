1ba05f96e00e8d593f973c17545226c36fde4a3f
Emma Forslund <emfo2102@student.miun.se>
2022-09-29 14:09:55 +0200

    Tagit bort quote pga missnöjd med utseendet

--- Moment 3 - Frontend-ramverk/src/App.vue
+++ Moment 3 - Frontend-ramverk/src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <Header />
-  <main>
-    <RouterView />
-  </main>
+  <div class="container">
+    <Header />
+    <main>
+      <RouterView />
+    </main>
+  </div>
   <Footer />
 </template>
 
@@ -11,22 +13,19 @@
 import { RouterLink, RouterView } from 'vue-router'
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
-import Quote from "./components/Quote.vue";
 export default {
   components: {
     Header,
     RouterView,
-    Footer,
-    Quote
+    Footer
   }
 }
 </script>
 
 <style scoped>
-  main{
-    max-width: 1300px;
-    width: 100%;
-    margin: auto;
-  }
-
+main {
+  max-width: 1300px;
+  width: 100%;
+  margin: auto;
+}
 </style>
