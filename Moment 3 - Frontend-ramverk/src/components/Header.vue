e6ef693fc7f0c72d7808e94f06a630596e530ca8
Emma Forslund <emfo2102@student.miun.se>
2022-09-29 10:46:20 +0200

    Lagt till Routerlinks i navigeringen

--- Moment 3 - Frontend-ramverk/src/components/Header.vue
+++ Moment 3 - Frontend-ramverk/src/components/Header.vue
@@ -1,11 +1,12 @@
 <template>
     <header>
-        <div class="header-container">
-            <router-link to="/">
-                <h1>Mina podcasts</h1>
-            </router-link>
-            <nav>
-                <ul>
+        <nav>
+            <div class="nav-wrapper white-text">
+                <router-link to="/">
+                    <h1 class="brand-logo">Mina podcasts</h1>
+                </router-link>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger right"><i class="material-icons">menu</i></a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li>
                         <router-link to="/mina-favoriter">Mina favoriter</router-link>
                     </li>
@@ -13,68 +14,40 @@
                         <router-link to="/om-uppgiften">Om uppgiften</router-link>
                     </li>
                 </ul>
-            </nav>
-        </div>
+            </div>
+        </nav>
+        <ul class="sidenav purple lighten-4" id="mobile-demo">
+            <li class="white-text">
+                <router-link to="/mina-favoriter">Mina favoriter</router-link>
+            </li>
+            <li class="white-text">
+                <router-link to="/om-uppgiften">Om uppgiften</router-link>
+            </li>
+        </ul>
+        <Quote />
         <img src="../assets/microphone.jpg" alt="mikrofon framför svart bakgrund">
     </header>
 
 </template>
 
 <script>
-import { RouterLink } from 'vue-router'
+import { RouterLink } from 'vue-router';
+import Quote from '../components/Quote.vue';
 export default {
     components: {
-        RouterLink
+        RouterLink,
+        Quote
     }
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, {});
+});
 </script>
 
 <style scoped>
-h1 {
-    font-size: 3.5rem;
-    color: white;
-    font-family: 'Manrope', sans-serif;
-    margin-top: 0;
-}
-
-a{
-    text-decoration: none;
-    color: white;
-}
-
-a:hover{
-    font-weight: bold;
-    color: #ea80fc;
-}
-
-/*Menyns list-element*/
-li {
-    display: inline;
-    font-family: 'Manrope', sans-serif;
-    font-size: 2rem;
-    color: white;
-    margin: 4rem 2rem;
-}
-
-ul {
-    list-style-type: none;
-}
-
-img {
-    max-width: 100vw;
-    width: 100%;
-    margin: auto;
-}
-/* Headers container */
-.header-container{
-    display: flex;
-    justify-content: space-between;
-    max-width: 1300px;
-    width: 100%;
-    margin: auto;
-}
-
-header{
-    margin: 1rem;
+.nav-wrapper {
+    background-color: #080a09;
 }
 </style>
