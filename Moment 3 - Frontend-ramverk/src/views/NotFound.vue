fb8e251a060be5f34dcdf38fbd95f04a31f77d2d
Emma Forslund <emfo2102@student.miun.se>
2022-10-03 13:03:30 +0200

    Lagt till CSS till sidan

--- Moment 3 - Frontend-ramverk/src/views/NotFound.vue
+++ Moment 3 - Frontend-ramverk/src/views/NotFound.vue
@@ -1,16 +1,32 @@
 <template>
     <div>
-        <p class="white-text">404 - Denna sida existerar inte</p>
+        <p class="white-text center-align"> <i class="large material-icons">mood_bad</i><br> 404 <br>Sidan du sökte
+            efter finns inte</p>
+        <div class="link">
+            <div class="white-text">
+                <router-link to="/" class="white-text">
+                    Tillbaka till startsidan
+                </router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
 p {
-    font-size: 2.5em;
+    font-size: 3.5em;
 }
 
-div{
-    position: relative;
-    bottom: 6em;
+.link{
+    max-width: 300px;
+    text-align: center;
+    margin: auto;
+    padding: 4em;
+    text-decoration: underline;
+    cursor: pointer;
+}
+
+.link:hover{
+    font-weight: bold;
 }
 </style>
