89e5511a5755279199ac8b03f1325cec9ca7b5ff
Emma Forslund <emfo2102@student.miun.se>
2022-10-03 11:01:50 +0200

    Utökat fält i tabellen

--- Moment 3 - Frontend-ramverk/src/views/FavoritesView.vue
+++ Moment 3 - Frontend-ramverk/src/views/FavoritesView.vue
@@ -1,6 +1,8 @@
+<!--View där podcastens skrivs ut och tabellen-->
 <template>
     <section id="table-section">
         <h2 class="white-text center-align">Mina favorit-podcast</h2>
+        <!--Tabell -->
         <table class="white-text purple darken-4 highlight responsive-table">
             <thead>
                 <tr>
@@ -8,14 +10,17 @@
                     <th>Kategori</th>
                     <th>Antal medlemmar</th>
                     <th>Släpps veckovis</th>
+                    <th>Ta bort</th>
+                    <th>Uppdatera</th>
                 </tr>
             </thead>
             <tbody>
 
-                <Podcast @delete="deletePodcast(podcast.id)" v-for="podcast in podcasts" :podcast="podcast"
-                    :key="podcast.id" />
+                <Podcast @getId="getId(podcast.id)" @delete="deletePodcast(podcast.id)" v-for="podcast in podcasts"
+                    :podcast="podcast" :key="podcast.id" />
             </tbody>
         </table>
+        <!--Text för bekräftelse att podcasten tagits bort skrivs ut-->
         <div id="deleted" class="white-text center-align" v-if="deleted">
             <p>{{deleted}}</p>
         </div>
@@ -34,7 +39,6 @@
 import Podcast from '../components/Favorite.vue';
 import AddPodcast from '../components/AddFavorite.vue';
 
-
 export default {
     data() {
         return {
@@ -47,10 +51,12 @@ export default {
         AddPodcast
     },
     methods: {
+        //Hämtar alla podcasts
         async getPodcasts() {
             const resp = await fetch("https://warm-chamber-88577.herokuapp.com/api/podcasts");
             const data = await resp.json();
             this.podcasts = data;
+            //Skriver ut "Ja" för true och "Nej" för false, istället för 1:or och 0:or
             for (let i = 0; i < data.length; i++) {
                 if (data[i].weekly == true) {
                     data[i].weekly = "Ja";
@@ -59,8 +65,10 @@ export default {
                 }
             }
         },
+        //Metod för att ta bort podcast
         async deletePodcast(id) {
             const resp = await fetch("https://warm-chamber-88577.herokuapp.com/api/podcasts/" + id, {
+                //Använder metoden DELETE
                 method: "DELETE",
                 headers: {
                     "Accept": "application/json",
@@ -70,8 +78,26 @@ export default {
 
             const data = await resp.json();
 
+            //Sparar meddelande från apiet. Meddelandet om att podcasten har tagits bort
             this.deleted = data[0];
+            //Hämtar podcasterna på nytt
             this.getPodcasts();
+        },
+        //Hämtar klickad podcast
+        async getId(id) {
+            const resp = await fetch("https://warm-chamber-88577.herokuapp.com/api/podcasts/" + id);
+            const data = await resp.json();
+            console.log(data);
+
+            let getName = data.name;
+            let getCat = data.category;
+            let getMember = data.members;
+            let getWeekly = data.weekly;
+
+            this.name = getName;
+            this.category = getCat;
+            this.members = getMember;
+            this.weekly = getWeekly;
         }
     },
     mounted() {
@@ -84,8 +110,7 @@ export default {
 
 
 <style scoped>
-    .section{
-        margin-top: 2em;
-    }
-
+.section {
+    margin-top: 2em;
+}
 </style>
