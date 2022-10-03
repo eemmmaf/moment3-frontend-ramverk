10dccb2d3b95c33ce49e35c3b2c89fe88cad3cac
Emma Forslund <emfo2102@student.miun.se>
2022-10-03 11:02:01 +0200

    Skapat enkel not found-view

---
+++ Moment 3 - Frontend-ramverk/src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p class="white-text">404 - Denna sida existerar inte</p>
+    </div>
+</template>
+
+<style scoped>
+p {
+    font-size: 2.5em;
+}
+
+div{
+    position: relative;
+    bottom: 6em;
+}
+</style>
