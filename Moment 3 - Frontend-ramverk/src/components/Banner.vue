2440a8606bffdbaea54b93a7d47cbedf9b3ecade
Emma Forslund <emfo2102@student.miun.se>
2022-10-03 11:00:50 +0200

    Placerat banner i en egen komponent

---
+++ Moment 3 - Frontend-ramverk/src/components/Banner.vue
@@ -0,0 +1,10 @@
+<!--Komponent för banner-->
+<template>
+    <img src="../assets/microphone.jpg" alt="mikrofon framför svart bakgrund">
+</template>
+
+<style scoped>
+img {
+    max-width: 100%;
+}
+</style>
